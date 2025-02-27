47839d8ef9069aca5acbfd4f779dbcd7fcd5ac45
unknown <reikirayuki@gmail.com>
2025-02-27 16:09:18 +0700

    m

--- dist/assets/index2-EHXwB2X_.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c}from"./index-CK_9spMF.js";const r={},t={class:"w-full min-h-screen bg-gradient-to-r from-gray-100 to-yellow-500"};function n(s,a){return o(),c("div",t)}const l=e(r,[["render",n]]);export{l as default};
